b82d956b263fe17b6624d7b86d98ccdb3a58e40f
karim <karim10zizou@gmail.com>
2023-12-21 19:31:29 -0800

    update

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -50,3 +50,5 @@ int convert_day(int month, int day)
 	}
 	return (day);
 }
+
+
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,3 +9,5 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* MAIN_H */
+
+
