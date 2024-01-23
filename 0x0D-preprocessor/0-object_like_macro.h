caa6ac52d21fd997008f6dab44be97a061a4c8fc
karim <karim10zizou@gmail.com>
2024-01-22 18:58:36 -0800

    tasks

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,23 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
+
+
+0x0D-preprocessor/02-main.c
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
